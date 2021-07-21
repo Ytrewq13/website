d821e390aadd754dcea6e3ac289f86ac3faf8bff
Sam Whitehead <sam.everythingcomputers@gmail.com>
2021-07-22 00:54:03 +0100

    Get layout "working"

---
+++ scripts/css-adjust.js
@@ -0,0 +1,3 @@
+topBar = document.getElementById('top-bar')
+document.getElementById('content').style['margin-top'] = topBar.clientHeight + "px";
+topBar.style.width = document.body.clientWidth + "px";
