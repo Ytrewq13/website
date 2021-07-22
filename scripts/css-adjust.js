f41ce3eb836eed811671d9869f83b1e52c89a184
Sam Whitehead <sam.everythingcomputers@gmail.com>
2021-07-22 22:44:00 +0100

    Begin work on adding standardised width classes

--- scripts/css-adjust.js
+++ scripts/css-adjust.js
@@ -1,3 +1,6 @@
-topBar = document.getElementById('top-bar')
+var HEADER_PADDING = 10;
+var HEADER_MARGIN = 15;
+
+var topBar = document.getElementById('top-bar')
 document.getElementById('content').style['margin-top'] = topBar.clientHeight + "px";
-topBar.style.width = document.body.clientWidth + "px";
+topBar.style.width = (document.body.clientWidth - 2*(HEADER_PADDING + HEADER_MARGIN)) + "px";
