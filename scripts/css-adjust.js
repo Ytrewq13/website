99a713b138ed09b6c8e5561b31d36265472ab2e4
Sam Whitehead <sam.everythingcomputers@gmail.com>
2021-07-24 12:29:16 +0100

    Start using the 'cols-n' classes for widths of elements.

--- scripts/css-adjust.js
+++ scripts/css-adjust.js
@@ -3,4 +3,4 @@ var HEADER_MARGIN = 15;
 
 var topBar = document.getElementById('top-bar')
 document.getElementById('content').style['margin-top'] = topBar.clientHeight + "px";
-topBar.style.width = (document.body.clientWidth - 2*(HEADER_PADDING + HEADER_MARGIN)) + "px";
+// topBar.style.width = (document.body.clientWidth - 2*(HEADER_PADDING + HEADER_MARGIN)) + "px";
